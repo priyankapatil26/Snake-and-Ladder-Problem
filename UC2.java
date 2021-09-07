ac370d833ed569e606c35b8ea859364a103eaf89
Priyanka Patil1 <priyanka.patil1@morningstar.com>
2021-09-08 00:19:23 +0530

    Player rolls the die

---
+++ UC2.java
@@ -0,0 +1,19 @@
+package snake;
+
+import java.util.Random;
+
+public class UC2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int PlayerPos=0;
+		Random rand = new Random();
+		int dicenum = 1 + rand.nextInt(6);
+		System.out.print(dicenum);
+		
+		
+		
+
+	}
+
+}
